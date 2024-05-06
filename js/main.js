5f797954c33103a40009b9e3de75335cfb35bb18
marco-carangelo <marcocarangelo2016@gmail.com>
2024-05-06 15:34:29 +0200

    add: index header

--- js/main.js
+++ js/main.js
@@ -7,6 +7,14 @@ const favoriteColor = prompt("Inserire il colore preferito");
 const num1 = prompt("Inserire il dividendo da cui generare la password");
 const num2 = prompt("Inserire il divisore da cui generare la password");
 
+// Assegnazioni temporanee per non inserire tutti valori ad ogni modifica del
+// const userName = '3';
+// const userLastName = '3';
+// const favoriteColor = '3';
+// const num1 = '3';
+// const num2 = '3';
+
+
 
 //Variables
 //let finalNumber = num1 / num2;
@@ -19,6 +27,8 @@ console.log(
 Nome: ${userName}
 Cognome: ${userLastName} 
 Colore Preferito: ${favoriteColor}
+Primo numero inserito: ${num1}
+Secondo numero inserito: ${num2}
 
 ***************************************************
 La Password generata è: ${userName + userLastName + favoriteColor + (num1 / num2)}`
