9e2ee7f7f3ff80c34b8f3a2f231a6a9445d6552b
marco-carangelo <marcocarangelo2016@gmail.com>
2024-05-06 13:32:40 +0200

    add: data acquisition and print in console

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-console.log("Prima prova console javascript")
+// Variable definition and input acquisition
+
+const userName = prompt("Inserire il nome");
+const userLastName = prompt("Inserire il cognome");
+const favoriteColor = prompt("Inserire il colore preferito");
+const finalNumber = 21;
+
+// Resulti visualization on the console
+
+console.log(
+    `************STAMPA RISULTATO SU CONSOLE************
+
+Nome: ${userName}
+Cognome: ${userLastName} 
+Colore Preferito: ${favoriteColor}
+
+***************************************************
+La Password generata è: ${userName + userLastName + favoriteColor + finalNumber}`
+)
+
